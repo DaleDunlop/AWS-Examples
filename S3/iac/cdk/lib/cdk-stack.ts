a2b4344d8acc93415c9fb347ab1d6985ab9e4a44
Dale Dunlop <Dalejohndunlop@gmail.com>
2024-03-22 15:42:36 +0000

    small changes

--- S3/iac/cdk/lib/cdk-stack.ts
+++ S3/iac/cdk/lib/cdk-stack.ts
@@ -1,12 +1,10 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // Use 'this' as the scope for resources within the stack
     new s3.Bucket(scope, 'MyBucket');
   }
 }
