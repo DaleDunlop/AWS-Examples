06959fdc1644d327ea80265fe286376771914e6e
Dale Dunlop <Dalejohndunlop@gmail.com>
2024-03-22 19:34:24 +0000

    pulumi

---
+++ S3/iac/pulumi/__main__.py
@@ -0,0 +1,13 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+from pulumi_aws import s3
+
+# Create an AWS resource (S3 Bucket)
+bucket = s3.Bucket('my-bucket')
+
+# Export the name of the bucket
+pulumi.export('bucket_name', bucket.id)
+export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
+export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+export AWS_DEFAULT_REGION=us-west-2
